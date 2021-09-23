a5ba877c304306c73e3034848bbab1633e62f361
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 14:42:26 +0200

    component changes

--- components/UI/UiProjects.vue
+++ components/UI/UiProjects.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <div class="w-full grid-cols-2 grid gap-4">
-            <div
+        <div class="w-full grid-cols-1 md:grid-cols-2 grid gap-4">
+            <a
             v-for="project in projects"
             :key="project"
-            class="border-b-2 border-primary-200 col-span-1 p-5">
-                <ui-h3>{{ project }}</ui-h3>
-            </div>
+            target="_blank"
+            :href="project.url"
+            :class="{'cursor-not-allowed': !project.url, 'hover:bg-gray-700': project.url}"
+            class="col-span-1 p-5 flex rounded-3xl bg-gray-800 transform transition-colors relative">
+                <ui-h3>{{ project.name }}</ui-h3><ui-badge class="ml-2" v-if="!project.url">Private</ui-badge>
+                <p class="absolute top-0 left-0" v-if="project.level >= 8">⭐</p>
+            </a>
         </div>
     </div>
 </template>
@@ -16,14 +20,56 @@ export default {
     data () {
         return {
             projects: [
-                'LBC Composites',
-                'Roughneck Marine',
-                'Ladyplanet',
-                'SocialGate',
-                'Apropos.jobs',
-                'Arfina Capital',
-                'Bixe.ch',
-                'Harley Davidson',
+                {
+                    name: 'Mini Restauration',
+                    url: 'https://instagram.com/jarvisthemini',
+                    level: 10
+                },
+                {
+                    name: 'LBC Composites',
+                    url: 'https://lbc-composites.com',
+                    level: 0
+                },
+                {
+                    name: 'Roughneck Marine',
+                    url: 'https://roughneck-marine.com',
+                    level: 0
+                },
+                {
+                    name: 'Ladyplanet',
+                    url: 'https://ladyplanet.ch',
+                    level: 0
+                },
+                {
+                    name: 'Evolve (cms)',
+                    url: 'https://racerfish.com/de/web-development',
+                    level: 0
+                },
+                {
+                    name: 'SocialGate (app)',
+                    url: null,
+                    level: 0
+
+                },
+                {
+                    name: 'Apropos.jobs (app)',
+                    url: null,
+                    level: 0
+
+                },
+                {
+                    name: 'Nitrous (cms)',
+                    url: 'https://racerfish.com/de/web-development',
+                    level: 0
+                },
+                // 'LBC Composites',
+                // 'Roughneck Marine',
+                // 'Ladyplanet',
+                // 'SocialGate',
+                // 'Apropos.jobs',
+                // 'Arfina Capital',
+                // 'Bixe.ch',
+                // 'Harley Davidson',
             ]
         }
     }
