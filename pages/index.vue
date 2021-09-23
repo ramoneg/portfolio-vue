51a5b8f9c3e69cd6f62e1b7b2257821f51498c86
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 09:41:51 +0200

    page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ui-hero>
+      <ui-h1>
+        I like to write <ui-fancy overlay icon="⚡">#fancy</ui-fancy> code
+      </ui-h1>
+      <div class="mt-5">
+        <nuxt-link to="#projects">
+          <ui-button>Projects</ui-button>
+        </nuxt-link>
+      </div>
+    </ui-hero>
+    <ui-projects id="projects">
+      <ui-h2>Projects</ui-h2>
+    </ui-projects>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+
+}
 </script>
