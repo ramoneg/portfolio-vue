3a0ad32e0e9b449b9f3eba4b60c7d7a5699bb1be
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 11:41:29 +0200

    multiple button styles

--- components/UI/UiButton.vue
+++ components/UI/UiButton.vue
@@ -1,12 +1,39 @@
 <template>
   <button class="relative z-10 active:scale-95 transform transition-transform">
-      <div class="bg-transparent text-gray-900 px-5 py-3 bg-gradient-to-r from-primary-400 to-yellow-300 shadow cursor-pointer uppercase font-bold z-10" v-if="$slots.default">
+      <div class="bg-transparent px-5 py-3 shadow cursor-pointer uppercase font-bold z-10 rounded" :class="style" v-if="$slots.default">
         <slot />
       </div>
-      <div class="absolute top-0 left-0 filter blur-xl opacity-60 bg-gradient-to-r from-primary-300 to-yellow-200 w-full h-full" style="z-index: -1;" />
+      <div class="absolute top-0 left-0 filter blur-xl opacity-60 w-full h-full" :class="overlay" style="z-index: -1;" />
   </button>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    buttonStyle: {
+      type: String,
+      default: 'primary'
+    }
+  },
+  data () {
+    return {
+      buttonStyles: {
+        primary: 'text-gray-900 bg-gradient-to-r from-primary-400 to-yellow-300',
+        'secondary': 'text-white bg-gradient-to-r from-gray-700 to-gray-800'
+      },
+      overlayStyles: {
+        primary: 'bg-gradient-to-r from-primary-300 to-yellow-200',
+        'secondary': 'text-white'
+      }
+    }
+  },
+  computed: {
+    style () {
+      return this.buttonStyles[this.buttonStyle]
+    },
+    overlay () {
+      return this.overlayStyles[this.buttonStyle]
+    }
+  }
+}
 </script>
