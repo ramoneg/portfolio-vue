1d76ee106fb0c96d47bd1a6d6731f2654b6cb448
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 09:41:56 +0200

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-primary-950 text-gray-200 min-h-screen w-full flex justify-center pb-8">
+    <div class="max-w-6xl w-full min-h-full">
+      <!-- <nav class="p-2 sm:p-8 lg:p-10 mb-2">
+        <nuxt-link to="/">
+          <ui-logo></ui-logo>
+        </nuxt-link>
+      </nav> -->
+      <div class="px-2 sm:px-8 lg:px-10">
+        <nuxt-child />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
