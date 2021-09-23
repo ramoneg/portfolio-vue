91d391943394c962c1093c5b2953dc5277b60f0d
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 15:03:15 +0200

    added dist folder

---
+++ dist/_nuxt/a2ebdd6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{207:function(t,e,n){"use strict";n.r(e);var l={},c=n(35),component=Object(c.a)(l,(function(){var t=this,e=t.$createElement;return(t._self._c||e)("h3",{staticClass:"text-xl md:text-2xl font-heading font-black uppercase text-gray-300"},[t._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
