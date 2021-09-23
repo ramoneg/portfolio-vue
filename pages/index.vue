e633c18588c2f2c9fa49ff7f25d219c39a3f56da
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 13:31:57 +0200

    added space helpers

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <ui-hero>
-      <ui-h1>
+      <ui-h1 class="spacer">
         I like to write <ui-fancy overlay icon="⚡">#fancy</ui-fancy> code
       </ui-h1>
-      <div class="mt-5">
+      <div>
         <nuxt-link to="/projects">
           <ui-button>Projects</ui-button>
         </nuxt-link>
       </div>
     </ui-hero>
     <ui-section>
+      <ui-h2 class="spacer">Skills</ui-h2>
       <ui-skills />
     </ui-section>
   </div>
