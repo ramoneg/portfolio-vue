20ea63d0ceb67756136a87e1712510455aabcae5
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 11:38:34 +0200

    skill filter component

---
+++ components/UI/UiSkills.vue
@@ -0,0 +1,120 @@
+<template>
+    <div>
+        <div class="mb-8">
+            <ui-button
+                v-for="category in categories"
+                :key="category"
+                @click.native="setFilter(category)"
+                :button-style="category == filter ?  'primary' : 'secondary'"
+                class="mr-4"
+                >
+                {{ category }}
+            </ui-button>
+        </div>
+        <div class="flex flex-wrap items-center">
+            <div v-for="skill in skills"
+                :key="skill.name"
+                class="flex relative rounded-2xl shadow mr-8 mb-8 px-4 py-2 border bg-gray-900 border-gray-700 transition-opacity"
+                :class="{'opacity-25': (filter != 'all' && skill.category != filter)}">
+                <ui-h3>{{ skill.name }}</ui-h3><small class="absolute top-0 right-0" v-if="skill.level >= 8">⭐</small>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            filter: 'all',
+            skills: [
+                {
+                    name: 'Laravel',
+                    category: 'backend',
+                    level: 10
+                },
+                {
+                    name: 'PHP',
+                    category: 'backend',
+                    level: 9
+                },
+                {
+                    name: 'SOLID Code',
+                    category: 'backend',
+                    level: 6
+                },
+                {
+                    name: 'Linux/Unix',
+                    category: 'Engineering',
+                    level: 7
+                },
+                {
+                    name: 'Networks',
+                    category: 'Engineering',
+                    level: 7
+                },
+                {
+                    name: 'Vue',
+                    category: 'Frontend',
+                    level: 8
+                },
+                {
+                    name: 'Javascript',
+                    category: 'Frontend',
+                    level: 8
+                },
+                {
+                    name: 'AWS',
+                    category: 'Engineering',
+                    level: 5
+                },
+                {
+                    name: 'Git',
+                    category: 'Other',
+                    level: 7
+                },
+                {
+                    name: 'Welding',
+                    category: 'Fancy',
+                    level: 7
+                },
+                {
+                    name: 'Music Production',
+                    category: 'Fancy',
+                    level: 8
+                },
+                {
+                    name: 'Thaiboxing',
+                    category: 'Fancy',
+                    level: 7
+                },
+                {
+                    name: 'Project Management',
+                    category: 'Other',
+                    level: 5
+                },
+            ],
+        }
+    },
+    methods: {
+        setFilter(filter) {
+            this.filter == filter
+                ? this.filter = null
+                : this.filter = filter
+        }
+    },
+    computed: {
+        categories () {
+            const categories = ['all']
+
+            this.skills.map(v => {
+                if (!categories.some(c => c == v.category)) {
+                    categories.push(v.category)
+                }
+            })
+
+            return categories
+        }
+    }
+}
+</script>
