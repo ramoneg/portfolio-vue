913f3e33217402b10e50d6d130580efd4b392e98
Ramon Egger <ramon@ramonegger.ch>
2021-09-23 14:41:40 +0200

    added badge component

---
+++ components/UI/UiBadge.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-2 h-6 flex items-center rounded-xl text-gray-900 bg-gradient-to-r from-primary-400 to-yellow-300 font-bold">
+      <small><slot /></small>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
