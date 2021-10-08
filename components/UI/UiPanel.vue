db7892e2ae5811c222299ebcbd92835c1eed44da
Ramon Egger <ramon@ramonegger.ch>
2021-10-08 17:23:12 +0200

    extracted logic

---
+++ components/UI/UiPanel.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full md:w-2/3 bg-gray-800 rounded-3xl border-primary-300 p-8 font-mono text-left">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
