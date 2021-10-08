0c54221a6517f74888ca2e408a18b7e0ded5193f
Ramon Egger <ramon@ramonegger.ch>
2021-10-08 17:24:37 +0200

    build

--- dist/_nuxt/4dd7b04.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{208:function(t,e,n){"use strict";n.r(e);var l={},c=n(35),component=Object(c.a)(l,(function(){var t=this,e=t.$createElement;return(t._self._c||e)("h2",{staticClass:"text-5xl font-heading font-black uppercase text-gray-200"},[t._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
