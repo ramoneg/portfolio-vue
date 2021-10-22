05f38035d4da7780022a9ca07ed11644ddf6b40f
Ramon Egger <ramon@ramonegger.ch>
2021-10-22 12:34:44 +0200

    design polishes

--- components/UI/UiMobileMenu.vue
+++ components/UI/UiMobileMenu.vue
@@ -8,7 +8,7 @@
             </div>
         </transition>
         <transition name="mobile-menu">
-            <div class="bg-gray-600 px-12 bg-opacity-90 h-screen w-screen top-0 left-0 absolute z-50 transition-all flex flex-row items-center justify-center" v-if="$store.state.menu.isOpen">
+            <div class="bg-gray-600 px-12 bg-opacity-90 h-screen w-screen top-0 left-0 absolute z-50 transition-all flex flex-row items-center justify-center select-none" v-if="$store.state.menu.isOpen">
                 <div class="text-center">
                     <ui-panel class="text-center shadow-xl flex flex-col space-y-8 text-4xl font-heading uppercase">
                         <nuxt-link v-for="link in $store.state.menu.links" :key="link.url" @click.native="$store.dispatch('menu/toggleMenu')" :to="link.url">{{ link.name }}</nuxt-link>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="bg-gradient-to-tr from-primary-950 to-gray-800 text-gray-200 min-h-screen w-full flex justify-center relative" :class="{'max-h-screen overflow-hidden': $store.state.menu.isOpen}">
-    <div class="fixed h-1 z-30 left-0 top-0 bg-gradient-to-r from-yellow-300 to-primary-300 transition-all duration-200" :style="{ width: `${$store.state.scroll.percent}%` }" />
+    <div class="fixed h-1 z-30 left-0 top-0 bg-gradient-to-r from-yellow-300 to-primary-300 transition-all duration-75" :style="{ width: `${$store.state.scroll.percent}%` }" />
     <nav class="z-20 w-full fixed left-0 top-0 flex justify-center transition-colors" :class="{'backdrop-filter backdrop-blur shadow-lg bg-opacity-30 bg-gray-700': !top}">
       <div class="max-w-6xl w-full">
-        <div class="px-8 lg:px-12 py-4 flex items-center justify-between">
-          <nuxt-link :to="$store.state.menu.links.find(v => v.name == 'Home').url">
+        <div class="px-8 lg:px-12 py-4 flex items-center justify-end">
+          <!-- <nuxt-link :to="$store.state.menu.links.find(v => v.name == 'Home').url">
             <ui-logo />
-          </nuxt-link>
+          </nuxt-link> -->
           <ui-mobile-menu class="lg:hidden" />
           <div class="justify-between hidden lg:flex text-gray-400">
               <nuxt-link class="ml-8 font-heading text-xl font-bold uppercase transition-colors" exact-active-class="text-white" v-for="link in $store.state.menu.links" :key="link.url" :to="link.url">{{ link.name }}</nuxt-link>
