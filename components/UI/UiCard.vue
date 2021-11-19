d6af09b07ec7467e810a581ff972190e7bd7bf52
Ramon Egger <ramon@ramonegger.ch>
2021-11-19 17:24:15 +0100

    top spacing for images

--- components/UI/UiCard.vue
+++ components/UI/UiCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rounded bg-gray-800 relative overflow-hidden shadow-xl">
         <div class="absolute w-full h-full pointer-events-none bg-gradient-to-tr via-transparent to-white z-10 opacity-30" />
-        <UiImage class="h-48 w-full object-contain" v-if="image" :image="image" />
+        <UiImage class="h-48 mt-4 w-full object-contain" v-if="image" :image="image" />
         <div class="p-4 flex flex-col space-y-4">
             <p class="font-bold">{{ title }}</p>
             <div v-if="$slots.default">
