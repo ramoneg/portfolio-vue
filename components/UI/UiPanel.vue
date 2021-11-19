a883cd51c49d0292aa0fe868a824ad69ca41821e
Ramon Egger <ramon@ramonegger.ch>
2021-11-19 17:24:07 +0100

    moved width class to layout

--- components/UI/UiPanel.vue
+++ components/UI/UiPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full md:w-2/3  bg-gradient-to-tr from-gray-800 to-gray-700 rounded-3xl border-primary-300 p-8 font-mono text-left">
+  <div class="w-full bg-gradient-to-tr from-gray-800 to-gray-700 rounded-3xl border-primary-300 p-8 font-mono text-left">
     <slot />
   </div>
 </template>
