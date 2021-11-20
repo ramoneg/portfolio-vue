d32bf7bc5b4bd801a26c331157e1a4d861021dfe
Ramon Egger <ramon@ramonegger.ch>
2021-11-21 00:38:18 +0100

    added button

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,11 @@
         I like to write <ui-fancy overlay icon="⚡" icon-size="text-9xl">#fancy</ui-fancy> code
       </h1>
       <div>
-        <nuxt-link to="/projects">
-          <UiButton>Projects</UiButton>
+        <nuxt-link to="/blog">
+          <UiButton>Blog</UiButton>
+        </nuxt-link>
+        <nuxt-link class="ml-4" to="/projects">
+          <UiButton :show-overlay="false" button-style="secondary">Projects</UiButton>
         </nuxt-link>
       </div>
     </UiHero>
